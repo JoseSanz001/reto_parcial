0022271d30237d9dd027d909d4ede252e747d572
Jose Sanchez <josesanchezflores012@gmail.com>
2025-10-26 09:23:23 -0500

    crea perfiles

--- my_project/core/apps.py
+++ my_project/core/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    
+    def ready(self):
+        """
+        Importar las señales cuando la app esté lista.
+        Esto asegura que los perfiles se creen automáticamente.
+        """
+        import core.signals
